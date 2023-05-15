66e85e6d69ceef46c47f52ec28ac9299b56bb391
Andre Korporaal <andrekorporaal@gmail.com>
2023-05-16 00:35:04 +0200

    Add mode shifting feature for greetings and add equal random greeting loader

--- prototype/ml_system.py
+++ prototype/ml_system.py
@@ -1,9 +1,12 @@
 import pickle
+import pandas as pd
 from transformers import pipeline
 from stt_system import SpeechRec
 from tts_system import text_to_speech
 from write_transcript import Transcriber
 from pathlib import Path
+import threading as th
+import time
 
 
 class LanguageModelSys():
@@ -23,6 +26,39 @@ class LanguageModelSys():
         return pipeline(ml_task, self.model_str)
 
 
+class ModeModerator():
+    def __init__(self) -> None:
+        pass
+
+    def _equal_rand(self, choices):
+        if len(choices.score.unique()) == 1:
+            choice = choices.sent.sample().to_string(index=False)
+            choices.loc[choices.sent == choice, 'score'] += 1
+        else:
+            choice = choices[choices.score < choices.score.max()].sent.sample().to_string(index=False)
+            choices.loc[choices.sent == choice, 'score'] = choices.score.max()
+
+        return choice, choices
+
+    def initiate_conv(self):
+        f_name = 'greeting.csv'
+        choices = pd.read_csv(f_name, sep='\t', names=['sent', 'score'])
+        choice, choices = self._equal_rand(choices)
+        choices.to_csv(f_name, sep='\t', header=False, index=False)
+
+        return choice
+
+    # def end_conv(self):
+        # choice_lst =
+        # self.tts(self._equal_rand(choice_lst))
+
+
+def thread_greeter(bot_state, n=10):
+    if bot_state[-1] < 1:
+        time.sleep(n)
+        bot_state[0] = 'greeting'
+
+
 def main():
     bot_name = 'blenderbot'
     if not Path(f'{bot_name}.pkl').is_file():
@@ -35,15 +71,36 @@ def main():
 
     speech_to_text = SpeechRec()
 
+    bot_state = ['main', -1]
+    thr = th.Thread(target=thread_greeter, args=(bot_state, 5), daemon=True)
+    thr.start()
+
     while True:
-        inp, timestamp = speech_to_text.listen()
-        script_transcriber.update_transcription(inp, timestamp, False)
+        while bot_state[0] == 'main':
+            bot_state[-1] += 1
 
-        out = blenderbot.chat(inp)[0]
-        timestamp = text_to_speech(out)
-        script_transcriber.update_transcription(out, timestamp)
+            inp, timestamp = speech_to_text.listen(bot_state)
+            if inp == None:
+                break
+            script_transcriber.update_transcription(inp, timestamp, False)
 
-        script_transcriber.convert_to_csv()
+            out = blenderbot.chat(inp)[0]
+            print('JAMMR: ', out)
+            timestamp = text_to_speech(out)
+            script_transcriber.update_transcription(out, timestamp)
+
+            script_transcriber.convert_to_csv()
+
+        if bot_state[0] == 'greeting':
+            bot_state[-1] += 1
+
+            out = ModeModerator().initiate_conv()
+            print('JAMMR: ', out)
+            timestamp = text_to_speech(out)
+            script_transcriber.update_transcription(out, timestamp)
+
+            script_transcriber.convert_to_csv()
+            bot_state[0] = 'main'
 
 
 if __name__ == '__main__':
--- prototype/stt_system.py
+++ prototype/stt_system.py
@@ -6,20 +6,21 @@ class SpeechRec():
     def __init__(self) -> None:
         self.r = sr.Recognizer()
 
-    def listen(self):
+    def listen(self, t):
 
         with sr.Microphone() as source:
-            print('Listening...')
+            # print('=== Listening...')
             self.r.adjust_for_ambient_noise(source)
             timestamp = datetime.now()
-            audio = self.r.listen(source, phrase_time_limit=5)
-
             try:
-                print('Recognizing...')
-                # query = self.r.recognize_whisper(audio, language='english')
+                if t[0] == 'greeting':
+                    return None, None
+                audio = self.r.listen(source, timeout=3, phrase_time_limit=5)
+
+                # print('=== Recognizing...')
                 query = self.r.recognize_google(audio, language='english')
-                # print(query.lstrip())
+
                 return query.lstrip(), timestamp
-            except Exception as e:
-                #print(e)
-                return self.listen()
+
+            except:
+                return self.listen(t)
